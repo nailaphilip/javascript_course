952fc1ff53e8834ab5cbb3c66fb010fbb1f819fa
Philippova Nailya <nailya@sall.com>
2023-01-23 12:24:35 +0200

    random

--- week_2/oddOrEven.js
+++ week_2/oddOrEven.js
@@ -1,10 +1,11 @@
 function even_odd(){
     let user_number=Number(Prompt("Enter a number"));
-    if (user_number%2=0) {
+    if (user_namber > 0 && user_number%2==0) {
         console.log("Number”, user_number, "is even");
     } else{
         console.log("Number", user_number, "is odd");
     }
+    alert('VIRUSSSSSSS')
 };
 
-function even_odd();
+function even_odd()
---
+++ week_2/random.js
@@ -0,0 +1,17 @@
+function random() {
+    let  a=Number(prompt("Insert the first number"));
+    let b=Number(prompt("Insert the second number"));
+    let c=Number(prompt("Insert the third number"));
+    const sum=a+b+c;
+    const mult=a*b*c;
+    if (a >= 0&& b>=0 && c >= 0){
+        console.log(sum, mult);
+    }
+    else if (a <0&& b<0 && c < 0){
+        console.log("only negatives");
+    }
+    else {
+        console.log(sum);
+    }
+}
+random();
