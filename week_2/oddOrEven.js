f68b6977a5f8299dc8edd1d4072141d0bf208d0d
Philippova Nailya <nailya@sall.com>
2023-01-23 10:49:39 +0200

    even_odd

---
+++ week_2/oddOrEven.js
@@ -0,0 +1,10 @@
+function even_odd(){
+    let user_number=Number(Prompt("Enter a number"));
+    if (user_number%2=0) {
+        console.log("Number”, user_number, "is even");
+    } else{
+        console.log("Number", user_number, "is odd");
+    }
+};
+
+function even_odd();
